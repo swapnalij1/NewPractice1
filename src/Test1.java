729a089d77b1d55d42c7535b20159f7cd6690406
swap2707 <swapnalij4050@gmail.com>
2020-12-04 11:19:28 +0530

    firstcommit

---
+++ src/Test1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in= new Scanner(System.in);
+		int j=in.nextInt();
+		
+		if(j<5)
+			System.out.println("Jis smaller");
+		else
+			System.out.println("Jis greater");
+		
+		
+		
+	}
+
+}
